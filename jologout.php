2d635ca17ace03c24502ab0aecc6f858088c5f77
Joya Yamashita <joppy617@icloud.com>
2018-06-19 05:38:21 +0900

    自習

---
+++ jologout.php
@@ -0,0 +1,14 @@
+<?php 
+    session_start();
+    if (ini_get('session.use_cookies')) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() = 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    }
+    session_destroy();
+
+    setcookie('email', '', time -3000);
+    setcookie('password', '', time -3000);
+
+    header('Location: joind.php');
+
+ ?>
